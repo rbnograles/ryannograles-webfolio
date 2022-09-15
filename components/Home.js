2ea9cac3992061a4e23fc60f6f495641fc6693c4
rbnograles <ryannograles.025.gtsi@gmail.com>
2022-09-15 10:20:12 +0800

    improve and refactor home page of webfolio

--- components/Home.js
+++ components/Home.js
@@ -4,22 +4,23 @@ import { Button } from "@mui/material";
 const Home = () => {
     return (
         <div className="flex-row-container">
-            <div className="container-left">
+            <div data-aos="fade-right" className="container-left">
                 <p>Hi, I am</p>
+                <div className="line"></div>
                 <h1>Ryan</h1>
-                <h1>Nograles</h1>
+                <h1>Nograles.</h1>
                 <p>
                     A <b>Software Engineer</b> based in the Philippines.
                 </p>
                 <Button variant="contained">Learn More</Button>
             </div>
-            <div className="container-right">
+            <div data-aos="fade-left" className="container-right">
                 <Image
                     src="/personWithLogos.png"
                     className="hero-image"
                     alt="Person with Logos"
-                    width="400px"
-                    height="400px"
+                    width="500px"
+                    height="500px"
                 />
             </div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,22 @@
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import Head from "next/head";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
 
 import NavBar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Home from "../components/Home";
 
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-
 const Main = () => {
+    useEffect(() => {
+        AOS.init({
+            duration: 2000,
+            easing: "ease",
+        });
+    }, []);
+
     return (
         <div className="container-relative">
             <Head>
