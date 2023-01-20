cf42842c0ef632e89417e59d3ab89f4892c35174
rbnograles <ryannograles.025.gtsi@gmail.com>
2023-01-20 10:06:00 +0800

    update class to className to remove compilation warning

--- components/Experience.js
+++ components/Experience.js
@@ -6,17 +6,17 @@ const Experience = () => {
             <section className="intro-left">
                 <h1>My Experiences</h1>
             </section>
-            <div class="wrapper">
-                <div class="center-line">
-                    <a href="#" class="scroll-icon">
+            <div className="wrapper">
+                <div className="center-line">
+                    <a href="#" className="scroll-icon">
                         <FaCaretDown />
                     </a>
                 </div>
-                <div class="row row-1">
+                <div className="row row-1">
                     <section data-aos="fade-right">
-                        <i class="icon fas fa-home"></i>
-                        <div class="details">
-                            <span class="title">Title of Section 1</span>
+                        <i className="icon fas fa-home"></i>
+                        <div className="details">
+                            <span className="title">Title of Section 1</span>
                             <span>1st Jan 2021</span>
                         </div>
                         <p>
@@ -25,17 +25,17 @@ const Experience = () => {
                             maiores, sint eos vero sapiente voluptas debitis
                             dicta dolore.
                         </p>
-                        <div class="bottom">
+                        <div className="bottom">
                             <a href="#">Read more</a>
                             <i>- Someone famous</i>
                         </div>
                     </section>
                 </div>
-                <div class="row row-2">
+                <div className="row row-2">
                     <section data-aos="fade-left">
-                        <i class="icon fas fa-star"></i>
-                        <div class="details">
-                            <span class="title">Title of Section 2</span>
+                        <i className="icon fas fa-star"></i>
+                        <div className="details">
+                            <span className="title">Title of Section 2</span>
                             <span>2nd Jan 2021</span>
                         </div>
                         <p>
@@ -44,17 +44,17 @@ const Experience = () => {
                             maiores, sint eos vero sapiente voluptas debitis
                             dicta dolore.
                         </p>
-                        <div class="bottom">
+                        <div className="bottom">
                             <a href="#">Read more</a>
                             <i>- Someone famous</i>
                         </div>
                     </section>
                 </div>
-                <div class="row row-1">
+                <div className="row row-1">
                     <section data-aos="fade-right">
-                        <i class="icon fas fa-rocket"></i>
-                        <div class="details">
-                            <span class="title">Title of Section 3</span>
+                        <i className="icon fas fa-rocket"></i>
+                        <div className="details">
+                            <span className="title">Title of Section 3</span>
                             <span>3rd Jan 2021</span>
                         </div>
                         <p>
@@ -63,17 +63,17 @@ const Experience = () => {
                             maiores, sint eos vero sapiente voluptas debitis
                             dicta dolore.
                         </p>
-                        <div class="bottom">
+                        <div className="bottom">
                             <a href="#">Read more</a>
                             <i>- Someone famous</i>
                         </div>
                     </section>
                 </div>
-                <div class="row row-2">
+                <div className="row row-2">
                     <section data-aos="fade-left">
-                        <i class="icon fas fa-globe"></i>
-                        <div class="details">
-                            <span class="title">Title of Section 4</span>
+                        <i className="icon fas fa-globe"></i>
+                        <div className="details">
+                            <span className="title">Title of Section 4</span>
                             <span>4th Jan 2021</span>
                         </div>
                         <p>
@@ -82,17 +82,17 @@ const Experience = () => {
                             maiores, sint eos vero sapiente voluptas debitis
                             dicta dolore.
                         </p>
-                        <div class="bottom">
+                        <div className="bottom">
                             <a href="#">Read more</a>
                             <i>- Someone famous</i>
                         </div>
                     </section>
                 </div>
-                <div class="row row-1">
+                <div className="row row-1">
                     <section data-aos="fade-right">
-                        <i class="icon fas fa-paper-plane"></i>
-                        <div class="details">
-                            <span class="title">Title of Section 5</span>
+                        <i className="icon fas fa-paper-plane"></i>
+                        <div className="details">
+                            <span className="title">Title of Section 5</span>
                             <span>5th Jan 2021</span>
                         </div>
                         <p>
@@ -101,17 +101,17 @@ const Experience = () => {
                             maiores, sint eos vero sapiente voluptas debitis
                             dicta dolore.
                         </p>
-                        <div class="bottom">
+                        <div className="bottom">
                             <a href="#">Read more</a>
                             <i>- Someone famous</i>
                         </div>
                     </section>
                 </div>
-                <div class="row row-2">
+                <div className="row row-2">
                     <section data-aos="fade-left">
-                        <i class="icon fas fa-map-marker-alt"></i>
-                        <div class="details">
-                            <span class="title">Title of Section 6</span>
+                        <i className="icon fas fa-map-marker-alt"></i>
+                        <div className="details">
+                            <span className="title">Title of Section 6</span>
                             <span>6th Jan 2021</span>
                         </div>
                         <p>
@@ -120,7 +120,7 @@ const Experience = () => {
                             maiores, sint eos vero sapiente voluptas debitis
                             dicta dolore.
                         </p>
-                        <div class="bottom">
+                        <div className="bottom">
                             <a href="#">Read more</a>
                             <i>- Someone famous</i>
                         </div>
